c964c09c96cf83ec5e85ed2f3e7adcd86c55d680
jelicpat <patrik.jelic@uni-a.de>
2026-02-23 09:56:42 +0100

    Custom exception class

--- exceptions.cpp
+++ exceptions.cpp
@@ -1,5 +1,7 @@
 #include "exceptions.h"
 
-void throw_error(const std::string& msg) {
-    throw std::invalid_argument(msg);
+DimensionErrorException::DimensionErrorException(const std::string& msg) : message(msg) { }
+
+const char* DimensionErrorException::what() const noexcept {
+    return this->message.c_str();
 };
--- exceptions.h
+++ exceptions.h
@@ -4,9 +4,14 @@
 #include <stdexcept>
 #include <string>      
 
-void throw_error(const std::string& msg);
+class DimensionErrorException : public std::exception {
+    public:
+        DimensionErrorException(const std::string& msg);
 
-// throw std::invalid_argument("Vectors have different dimensions of " + std::to_string(v1.dimension()) + " and " + std::to_string(v2.dimension()));
+        const char* what() const noexcept override;
 
+    private:
+        std::string message;
+};
 
 #endif
